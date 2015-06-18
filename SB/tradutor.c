d80737e4f8acb4134792940b201008aa120d5af7
Notadog <danilosantos_23@hotmail.com>
2015-06-18 13:22:18 -0300

    Organizando o arquivo para manter minha sanidade. Tambem transformei o arquivo que fazia a conversão <assembly inventado> -> <assembly IA-32> em uma biblioteca, e tirei os avisos de erros já que a função análise identifica eles.

--- SB/tradutor.c
+++ SB/tradutor.c
@@ -1,12 +1,15 @@
-#include "bib.h"
-#include "preproc.h"
-#include "parser.h"
-#include "scanner.h"
-#include "traducaoIA32.h"
+#include "Bibliotecas/bib.h"
+#include "Bibliotecas/preproc.h"
+#include "Bibliotecas/parser.h"
+#include "Bibliotecas/scanner.h"
+
+#include "Bibliotecas/traducaoIA32.h"
+
+/*Função que eu quero construir antes de mover para uma biblioteca*/
+void sintese_IA32 (char *src, char *dst);
 
 int main (int argc,char *argv[]) {
 
-	FILE *entrada=NULL, *saida=NULL;
 	SYMBOL_TABLE *comeco_tabela=NULL;
 
 	if (!valida_linhacomando(argc, argv))
@@ -16,16 +19,33 @@ int main (int argc,char *argv[]) {
 	pre_processamento(argv[1], PREPROCESSED_FILE);
 
 	/*Faz a análise do código já pré-processado*/
-	entrada = fopen(PREPROCESSED_FILE, "r");
-	analise(entrada, comeco_tabela);
+	analise(PREPROCESSED_FILE, comeco_tabela);
+
+	/*Se encontrar erros nem passa para a próxima fase*/
+	if (error_count != 0){
+		remove(PREPROCESSED_FILE);
+		return 0;
+	}
 
 	/*
-	sintese (codigo máquina inventado)
-	sintese (codigo IA32)
+	sintese (linguagem IA32)
+	sintese (codigo maquina IA32)
 	sintese ELF
 	*/
 
 	/*DEPOIS QUE JÁ FEZ O QUE TINHA QUE FAZER*/
-	//remove(PREPROCESSED_FILE);
+	remove(PREPROCESSED_FILE);
 	return 0;
+}
+
+void sintese_linguagem (char *src_name, char *dst_name) {
+
+	FILE *src, *dst;
+
+	src = fopen(src_name, "r");
+	dst = fopen(dst_name, "w");
+
+	fclose(src);
+	fclose(dst);
+
 }
